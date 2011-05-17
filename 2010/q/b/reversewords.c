032673db61c27e003e48da097c49a8466cb64a6b
Javier Ubillos <javvee@gmail.com>
2011-05-17 09:30:58 +0200

    Solved the reversewords (small) problem. (2010/q/b). Large still has a problem, inserts non-ascii symbol.

--- 2010/q/b/reversewords.c
+++ 2010/q/b/reversewords.c
@@ -2,14 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-void double_buff(int *size, char **buff, char **half) {
-	char *tmp;
-	*size *= 2;
-	tmp = (char *)realloc(*buff, *size);
-	*buff = tmp;
-	*half = *buff + *size/2;
-	return;
-}
 
 void read_line_to_buff(FILE *file, char **in_string){
 	int size = 1, read_chars = 0, i = 1;
@@ -19,84 +11,90 @@ void read_line_to_buff(FILE *file, char **in_string){
 	char *last_char;
 
 	*in_string = (char*) malloc(size * sizeof(char));
-	do {
-		double_buff(&size, in_string, &half);
-		do{
-			(*in_string)[read_chars] = getc(stdin);
-			last_char = &(*in_string)[read_chars];
-			read_chars += 1;
-		}while (read_chars < size && '\n' != *last_char && EOF != *last_char);
+	do{
+		(*in_string)[read_chars] = getc(stdin);
+		last_char = &(*in_string)[read_chars];
+		read_chars += 1;
+	}while ( '\n' != *last_char && EOF != *last_char);
 
-	} while( '\n' != *last_char && EOF != *last_char);
 	*last_char = '\0';
 	
-	fprintf(stderr, "read: %s\n", *in_string);
+	fprintf(stderr, "read_line_to_buff(): returning %s\n", *in_string);
 
 	return;
 }
 
-void reverse_words_in_buff(char **in_string){
-	char *string = *in_string;
-	char **words;
+
+char* reverse_word(char* word){
+	int len = 0;
+	int j = 0, i = 0;
+	char l;
+
+	//fprintf(stderr, "reverse_word( %s )\n", word);	
+	
+	if( 0 == word) return 0;
+	
+	while( ' ' != word[i] && '\0' != word[i] ) {
+		i++;
+	}
+	if('\0' == word[i]) word[i] = ' ';
+	len = i-1;
+	i -= 1;
+	
+	for(i; i > len/2; i--){
+		//fprintf(stderr, "reverse_word() (inloop) : i:%d, len:%d, len/2:%d\n", i, len, len/2);	
+		//fprintf(stderr, "reverse_char word[%d] %c <=> word[%d] %c\n", i, word[i], len-i, word[len-i]);	
+		l = word[i];
+		word[i] = word[len-i];
+		word[len-i] = l;
+	}
+
+	//fprintf(stderr, "reverse_word(): return %s\n", word);		
+	return word;
+}
+
+char* reverse_words_in_buff(char **in_string){
+	char *string;
 	char *word;
-	int words_size = 1 , i = 0;
-	char **tmp_words ;
-	char *wrt_pointer = 0, *str_out;
-	fprintf(stderr, "string: %s\n", string);
-	fprintf(stderr, "string: %p, wrt_pointer: %p\n", string, wrt_pointer);
-	words = malloc( words_size * sizeof(char*));
-	wrt_pointer = string;
+	char *word_end;
+	char tmp_char;
+	int i;
+	int len;
+	// Reverse string
 
+	string = *in_string;
+	fprintf(stderr, "reverse_words_in_buff( %s );\n", string);
+	
+	fprintf(stderr, "string_start: %c, string_end: %c, string: %s\n", string[0], string[strlen(string)-1],string);
+	len =  strlen(*in_string);
+	for(i = 0; i < len/2; i++) {
+		tmp_char = string[i];
+		string[i] = string[len - i - 1];
+		string[len - i - 1] = tmp_char;
+	}
+	fprintf(stderr, "string_start: %c, string_end: %c, string: %s\n", string[0], string[strlen(string)-1],string);
+
+	// Reverse words
+	word = 0;
 	word = strtok(string, " ");
-	fprintf(stderr, "word (%p): %s\n", word, word);
-	words[i] = word;
-
-	i += 1;
-	words_size += sizeof(char*);;
-	tmp_words = realloc(words, words_size);
-	words = tmp_words;
-
-	words[i] = word;
-	while (0 != word){
+	while( 0 != reverse_word(word) ){
 		word = strtok(0, " ");
-		
-		fprintf(stderr, "word (%p): %s\n", word, word);
-		words[i] = word;
-		i += 1;
-
-		words_size += sizeof(char*);		
-		tmp_words = realloc(words, words_size);
-		words = tmp_words;
 	}
-	i -= 2;
-	fprintf(stderr, "in conclusion\n");
-	str_out = wrt_pointer = malloc(strlen(string));
-	for(i  ; i >= 0 ; i--) {
-		fprintf(stderr, "word (%p): %s\n", words[i], words[i]);
-		sprintf(wrt_pointer, "%s", words[i]);
-		wrt_pointer += strlen(words[i]);
-		if(0 < i) {
-			fprintf(stderr, "space <= %s\n", wrt_pointer);
-			sprintf(wrt_pointer, " ");
-			wrt_pointer += 1;
-		}
+	
+	string[strlen(string)-1] = '\0';	
+	fprintf(stderr, "string_start (%p): %c, string_end (%p): %c, string: \"%s\"\n", string, string[0], &string[strlen(string)-1], string[strlen(string)-1],string);
 
+	fprintf(stderr, "reverse_words_in_buff(): return %s\n", string);
+	fprintf(stderr, "reverse_words_in_buff(): return %s\n", *in_string);
 
-	}
-	*(wrt_pointer+1) = '\0';
-
-	fprintf(stderr, "out: %s\n", str_out);
-	*in_string = str_out;
-
-	free(words);
-	return;
+	return *in_string;
 }
 
 int main(int argc, char *argv[])
 {
 	
 	int no_test_cases, i;
-	char* in_string;
+	char in_string[1001];
        
 	if(argc != 1) {
 		printf("Usage:   %s > <out-file>  < <in-file> \n" , argv[0]);
@@ -106,12 +104,13 @@ int main(int argc, char *argv[])
 	fscanf(stdin, "%d\n", &no_test_cases);
 	
 	for(i = 0; i < no_test_cases; i++) {
-		in_string = malloc(1);
-		fprintf(stderr, "******** Case #%d: \n" , i+1);
+		memset(in_string, 0, sizeof(in_string));
+//		fprintf(stderr, "in_string: %p, *in_string: %p, **in_string: %s\n", in_string, *in_string, **in_string);
 		read_line_to_buff(stdin, &in_string);
-		reverse_words_in_buff(&in_string);
-		printf("Case #%d: %s\n" , i+1, in_string);
-		free(in_string);
+//		fprintf(stderr, "in_string: %p, *in_string: %p, **in_string: %s\n", in_string, *in_string, **in_string);
+		printf("Case #%d: %s\n" , i+1,reverse_words_in_buff(&in_string) );
+//		fprintf(stderr, "in_string: %p, *in_string: %p, **in_string: %s\n", in_string, *in_string, **in_string);
+
 	}
 	
 	
